9cff986831bbc24fb83554b218d1101fc1d37460
JOAO PEDRO BATISTA DOS SANTOS <jo20272584@wipro.com>
2022-06-13 09:33:10 -0300

    Enviando atualizações...

--- promises/promises_1.js
+++ promises/promises_1.js
@@ -3,11 +3,13 @@
 
 
 let p = new Promise(function (cumprirPromessa) {
+    //se passa diferentes tipos de valores, string, numero, arrey, objeto, lista
     cumprirPromessa(3);
 })
-/*  para executar o objeto p se usa o then passando uma função com parâmetro de valor que vai receber o valor do parâmetro
-    da função de promise
-*/
+/*  para executar o objeto p se usa o then passando uma função como parâmetro
+ *  com parâmetro de valor que vai receber o valor do parâmetro
+ *  da função de promise
+ */
 p.then(function (valor) {
     //nesse caso o valor de valor vai ser 3
     console.log(valor);
